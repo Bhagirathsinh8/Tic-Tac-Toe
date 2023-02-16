564e446c5fdcd82762dabeb9be91e36a48a45521
Bhagirathsinh8 <122227146+Bhagirathsinh8@users.noreply.github.com>
2023-02-17 00:12:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Welcome");
+
+let turn = "X"
+let gameOver = false;
+
+//function to chnage the turn
+const changeTurn = ()=>{
+
+    return turn ==="X"?"0": "X"
+}
+
+//function to check win
+const checkWin = ()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+        wins.forEach(e =>{
+            if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText)  && (boxtext[e[0]].innerText === boxtext[e[0]].innerText !==""));
+            document.querySelector('.info').innerText = boxtext[e[0]].innerText + "Won";
+            gameOver = true;
+         })
+}
+
+//main game
+let boxes = document.getElementsByClassName('box');
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=>{
+        if(boxtext.innerText=== ''){
+            boxtext.innerText = turn;
+            turn = changeTurn();
+            checkWin();
+            if (!gameOver) {
+                document.getElementsByClassName("info")[0].innerText = "Turn for  " + turn;
+            }
+        }
+    })
+})
